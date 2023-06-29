36f78ca3d811f891a8f139655f369d19e587be81
ImeneRdn <imene.ramdan@live.fr>
2023-06-29 14:13:33 +0200

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import DatabaseList from "./Components/DatabaseList";
 import Home from "./pages/Home";
 import "./main.css";
-import Login from "./Components/login/Login";
 
 function App() {
   return (
     <div>
       <Home />
-      <Login />
+
       <Routes>
         <Route path="/databaselist" element={<DatabaseList />} />
       </Routes>
