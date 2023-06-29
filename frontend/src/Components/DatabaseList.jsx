cea026cb1de62969425650c0c244d142c03d5f60
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-06-29 09:27:09 +0200

    database list component + install router dom

--- frontend/src/Components/DatabaseList.jsx
+++ frontend/src/Components/DatabaseList.jsx
@@ -5,9 +5,9 @@ function DatabaseList() {
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/products`)
-      .then((response) => {
-        /* console.log(response.json()); */
-        setSmartphones(response);
+      .then((response) => response.json())
+      .then((data) => {
+        setSmartphones(data);
       })
       .catch((err) => {
         console.error(err);
@@ -15,22 +15,29 @@ function DatabaseList() {
   }, []);
 
   return (
-    <div>
-      {smartphones.length > 0 &&
-        smartphones.map((smartphone) => (
-          <>
-            <img
-              key={smartphone.id}
-              src={smartphone.phone_img}
-              alt="smartphone"
-            />
-            <h3>
-              {smartphone.brand} {smartphone.model}
-            </h3>
-            <h4>{smartphone.price}</h4>
-            <p>{smartphone.phone_condition}</p>
-          </>
-        ))}
+    <div className="flex justify-center">
+      <div className="gap-4 space-y-4">
+        {smartphones.length > 0 &&
+          smartphones.map((smartphone) => (
+            <div
+              key={smartphone.product_id}
+              className="bg-white rounded-lg p-4 shadow-md max-w-sm mx-auto h-80 "
+            >
+              <div className="flex justify-center items-center">
+                <img
+                  src={smartphone.phone_img}
+                  alt="smartphone"
+                  className="w-40 h-auto object-contain mb-2"
+                />
+              </div>
+              <h3 className="text-xl font-bold mb-1">
+                {smartphone.brand} {smartphone.model}
+              </h3>
+              <h4 className="text-lg font-medium mb-1">{smartphone.price}</h4>
+              <p className="text-gray-500">{smartphone.phone_condition}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
