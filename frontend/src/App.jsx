f18785d6c40afa6de317c19ae58cb891e9089050
Bascom3000 <112181817+Bascom3000@users.noreply.github.com>
2023-06-29 22:06:02 +0200

    nouveauProd algo ok + props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import PhoneForm from "./Components/NouveauProd";
 import "./main.css";
 import Login from "./Components/login/Login";
+import MobileResume from "./Components/MobileResume";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/nouveauproduit" element={<PhoneForm />} />
         <Route path="/databaselist" element={<Database />} />
+        <Route path="/resume" element={<MobileResume />} />
       </Routes>
     </div>
   );
